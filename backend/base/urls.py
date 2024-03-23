240f0833a72a8632293e22737d9f76aa4e32aabf
zfelcyn <zachary.felcyn@wsu.edu>
2024-03-22 18:39:47 -0700

    Updated Backend with Rest

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('messages/', views.getMessages, name="messages"),
+    path('messages/<str:pk>', views.getMessage, name="message"),
 ]
