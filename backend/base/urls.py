c8c7a0916ea60e15f2937c39cb689b0472dd22d1
zfelcyn <zachary.felcyn@wsu.edu>
2024-04-02 18:26:25 -0700

    Zack Moved over to My Branch
    Also Uploaded Cite, https://yikclonewsu2024.pythonanywhere.com/

--- backend/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views 
-
-
-urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('messages/', views.getMessages, name="messages"),
-    path('messages/<str:pk>', views.getMessage, name="message"),
-]
---
+++ djangochat/chat/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path # imports path funmction from django library
+from . import views # imports views from local files, each view functionr returns an http result
+
+# url patterns are something that django uses to route incomming http requests to the appropritate view
+# based on the requests url path. An http request is a message sent by a client to a server,\
+# asking for some action to be performed on the server. Each http request includes a method, like  the action type(GET to submit data, PUT to update resoure, and DELETE to remove stuff)
+# URL, or uniform resource locator, specifies the path to the resource on the server, headers provide additional info like content type, authetication tokens, ect. Finally theres the body, which contains data sent
+# with the request.
+
+# A view in context of django, is a python function that takes an http request as input and returns an http result
+# Logic in this can include accessing data from the database, processing or transforming data, or deciding what to send back, like a html page,
+# json, xml, redirect, whatever. A VIEW FUNCTION IS THE AIR TRAFFIC CONTROL OF THE BACKEND 
+urlpatterns = [
+    # matches an empty string representing the root URL of the website, and route those requests to the home function
+    # in the views module. The name = 'home' assigns a name to the url, which we can type in the website url
+    path('', views.home, name='home'),
+    # dynamic segment <str:room> matches any string and passes a room argum,ent, hekpful for creating urls based on username
+    path('<str:room>/', views.room,name='room'),
+    # path that leads to checkview, will explain this more in .views
+    path('checkview', views.checkview, name='checkview'),
+    # also for the send function, same stuff
+    path('send', views.send, name='send'),
+    # dynamic url pattern that fetches messages for a single room
+    path('getMessages/<str:room>/', views.getMessages,name='getMessages'),
+
+]
