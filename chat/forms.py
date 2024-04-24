5b552614fa7cf62336a82f3d6c403645b7c5a6d6
Zelefant <zelefantplaysmc@outlook.com>
2024-04-24 15:08:17 -0700

    Major changes to register page, added error messages to both login and register pages

--- chat/forms.py
+++ chat/forms.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class CustomAuthenticationForm(AuthenticationForm):
